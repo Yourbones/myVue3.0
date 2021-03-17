e07738a32fde6bd6ac175a4552015b0e8907c0c5
许浩星 <xuhaoxing9@163.com>
2021-03-17 15:10:40 +0800

    feat：二次封装axios请求实例

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { store, key } from './store'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
-import axios, { AxiosInstance } from 'axios'
+import { AxiosInstance } from 'axios'
+// 二次封装的 axios 实例
+import http from './utils/request'
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
@@ -18,6 +20,6 @@ app.use(store, key)
 app.use(router)
 app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 
-app.config.globalProperties.$http = axios
+app.config.globalProperties.$http = http
 
 app.mount('#app')
---
+++ src/utils/request.ts
@@ -0,0 +1,64 @@
+import axios, { AxiosInstance } from 'axios'
+import LoadingManager from './loading-manager'
+import { handleError } from './error-handler'
+import router from '../router'
+
+declare module 'axios' {
+  interface AxiosRequestConfig {
+    isLoading?: boolean;
+    catchError?: boolean;
+  }
+}
+
+const http: AxiosInstance = axios.create({
+  baseURL: (import.meta.env.NODE_ENV === 'development' ? '' : import.meta.env.VUE_APP_API_ROOT) as string,
+  isLoading: true,  // 是否加载全局 loading
+  catchError: true, // 是否统一处理异常
+})
+
+// 请求拦截器
+http.interceptors.request.use(
+  config => {
+    // 默认加载全局 loading，若不需要加载则手动设置 isLoading 为 false
+    // 例如：http.get(url, { isLoading: false })
+    config.isLoading && LoadingManager.showFullSrceenLoading()
+    config.headers['Authorization'] = localStorage.getItem('token') ? `Base ${localStorage.getItem('token')}` : ''
+
+    return config
+  },
+  error => {
+    // 关闭全局 loading
+    error.response.config.isLoading && LoadingManager.hideFullScreenLoading()
+
+    return handleError(error.response)
+  }
+)
+
+// 响应拦截器
+http.interceptors.response.use(
+  response => {
+    // 关闭全局 loading
+    response.config.isLoading && LoadingManager.hideFullScreenLoading()
+
+    if (response.data.rc === 0) {
+      // 请求成功
+      return response.data
+    }
+    // 请求失败
+    return handleError(response)
+  },
+  error => {
+    // 关闭全局 loading
+    error.response.config.isLoading && LoadingManager.hideFullScreenLoading()
+
+    // 对响应错误做点什么
+    let status = error.response.status
+    if (status === 401) {
+      // 跳转登录页
+      router.push({ name: 'login' })
+    }
+    return handleError(error.response)
+  }
+)
+
+export default http
