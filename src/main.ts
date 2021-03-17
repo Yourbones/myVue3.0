0dd85a31c093219d7a725259e341cf1fca3a27a3
许浩星 <xuhaoxing9@163.com>
2021-03-17 10:25:12 +0800

    feat：引入Element Plus

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { store, key } from './store'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
 const app = createApp(App)
 
 app.use(store, key)
 app.use(router)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 
 app.mount('#app')
