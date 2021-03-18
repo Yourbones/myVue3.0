8c176d91b613b95ff747fa0228ce906b0403db5c
许浩星 <xuhaoxing9@163.com>
2021-03-18 16:19:23 +0800

    feat：添加Layout、Home、Login页面视图文件

---
+++ src/views/layout/index.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: wy
+ * @LastEditors: Please set LastEditors
+ * @Description: 基础页
+-->
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+}
+</script>
+
+<style lang="scss" scoped></style>
