7feadc78267efae6a4f4d1bbad3d74db78558614
许浩星 <xuhaoxing9@163.com>
2021-03-19 17:40:02 +0800

    feat：配置接口请求代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 const pathSrc = path.resolve(__dirname, "./src");
+
+const VITE_APP_API_ROOT: string = 'http://116.62.9.135:5124/api'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +11,31 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      '/efficacy': {
+        target: VITE_APP_API_ROOT,
+        changeOrigin: true,
+      },
+      '/sysDictionary': {
+        target: VITE_APP_API_ROOT,
+        changeOrigin: true,
+      },
+      '/demandPool': {
+        target: VITE_APP_API_ROOT,
+        changeOrigin: true,
+      },
+      '/user': {
+        target: VITE_APP_API_ROOT,
+        changeOrigin: true,
+      },
+    },
+    hmr: {
+      overlay: false,
     },
   },
   css: {
