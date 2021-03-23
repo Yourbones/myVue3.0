e6f48218d0eee243588e684eaef21efb98a929af
许浩星 <xuhaoxing9@163.com>
2021-03-23 14:39:38 +0800

    feat：App.vue页面修改

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <template>
-  <div id="app-wraper">
-    <router-view></router-view>
+  <div id="app">
+    <router-view />
   </div>
 </template>
 
-<script lang="ts">
-  import { defineComponent } from 'vue'
-
-  export default defineComponent({
-    name: 'App',
-  })
-</script>
-
-<style>
-  #app-wraper {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-</style>
+<style lang="scss"></style>
